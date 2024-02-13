3e07a6f289332a3e877cb0896ab168b3b3855175
nevaeh <xiny7803@gmail.com>
2024-02-13 15:50:09 +1300

    build clciking animation for headers.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { links } from "@/lib/data"
 import Link from 'next/link'
+import clsx from "clsx";
 
 const Header = () => {
+    const [ activeSection, setActiveSection ] = useState("Home");
+
   return (
     <header className='z-[999] relative'>
         <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full 
@@ -19,13 +22,26 @@ const Header = () => {
              sm:top-[1.7rem] sm:h-[initial] sm:py-0'
             >
             <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem]
-            font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+            font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 '>
                 {
                     links.map((link)=> (
-                        <motion.li className='flex h-3/4 items-center justify-center' key={link.name}
+                        <motion.li className='flex h-3/4 items-center justify-center relative' key={link.name}
                         initial={{ y: -100, opacity: 0}}
                         animate={{ y: 0,  opacity: 1}}>
-                            <Link className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition' href={link.hash}>{link.name}</Link>
+                            <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition', {
+                                'text-gray-950': activeSection === link.name
+                            })} href={link.hash} onClick={() => setActiveSection(link.name)}>
+                                {link.name}
+                                {link.name === activeSection && (<motion.span className='bg-gray-200 rounded-full absolute inset-0 -z-10'
+                                layoutId='activeSection'
+                                transition={{
+                                    type:"spring",
+                                    stiffness: 380,
+                                    damping: 30
+                                }}>
+
+                                </motion.span>)}
+                                </Link>
                             </motion.li>
                     ))
                 }
