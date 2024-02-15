0a7829242cabf03f4e8ef127292d48cb4f7f6827
nevaeh <xiny7803@gmail.com>
2024-02-16 09:29:12 +1300

    adjust the responsiveness.

--- components/Project.tsx
+++ components/Project.tsx
@@ -25,12 +25,12 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
     }}>
     <section
       className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden
-        sm:pr-8 relative sm:h-[20rem] group-odd:pl-8 hover:bg-gray-200 transition rounded-lg
+        sm:pr-8 relative sm:h-[20rem] sm:group-odd:pl-8 hover:bg-gray-200 transition rounded-lg
          dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
     >
       <div
         className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full
-        group-odd:ml-[18rem]"
+        sm:group-odd:ml-[18rem]"
       >
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
@@ -49,7 +49,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
         src={imageUrl}
         alt="Project I worked on"
         quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        className="hidden sm:block absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
           group-odd:right-[initial] group-odd:-left-40
            transition group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.05]
            group-odd:group-hover:translate-x-3 group-odd:group-hover:translate-y-3 group-odd:group-hover:rotate-2
