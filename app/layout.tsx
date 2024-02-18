e760fbeeecdf44c347e23db694ad1817e552874f
nevaeh <xiny7803@gmail.com>
2024-02-19 01:17:47 +1300

    modify dark mode color.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,11 +28,11 @@ export default function RootLayout({
       >
         <div
           className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] sm:right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[8rem] sm:blur-[10rem] sm:w-[68.75rem]
-         dark:bg-[#946263]"
+         dark:bg-[#946263] dark:right-[11rem]"
         ></div>
         <div
           className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-12rem] sm:left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[8rem] sm:blur-[10rem] sm:w-[68.75rem]
-        md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
+        md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394] dark:left-[-35rem]"
         ></div>
         <ActiveSectionContextProvider>
           <ThemeContextProvider>
